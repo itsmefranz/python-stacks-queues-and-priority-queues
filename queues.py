eb863417c6f0c1f614090417e855af20cdfb065d
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-13 20:42:22 +0800

    added a new class Iterable

--- queues.py
+++ queues.py
@@ -2,20 +2,10 @@
 
 from collections import deque
 
-class Queue:
-    def _init_(self, *elements):
-        self.elements = deque(elements)
-    
+class Iterable:
     def _len_(self):
-        return len(self._elements)
+        return len(self.elements)
 
     def _iter_(self):
         while len(self) > 0:
             yield self.dequeue()
-
-    def enqueue(self, element):
-        self.elements.append(element)
-
-    def dequeue(self):
-        return self.elements.popleft()
-
