7d81d748d5077ed9f8f0ad543358e408e682478f
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-13 13:01:50 +0800

    defined append and popleft deque within the Queue class

--- queue-data-type/elements.py
+++ queue-data-type/elements.py
@@ -5,3 +5,9 @@ from collections import deque
 class Queue:
     def _init_(self):
         self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def deque(self):
+        return self._elements.popleft()
