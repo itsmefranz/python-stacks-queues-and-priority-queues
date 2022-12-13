fdfe8dcfd76416825afeed38a5c0b36dbe2abd19
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-14 03:55:54 +0800

    implementing the python list version as stack

--- interactive-verify-stack.py
+++ interactive-verify-stack.py
@@ -3,3 +3,11 @@ from queues import Stack
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
     print(element)
+
+# using python list as a rudimentary stack
+
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
