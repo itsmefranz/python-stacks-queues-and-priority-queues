8c3c5dc5fb1d7ff2392398bccc4d2eabc9571af6
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-13 19:43:16 +0800

    defined operations under the queue class

--- queues.py
+++ queues.py
@@ -3,3 +3,11 @@
 from collections import deque
 
 class Queue:
+    def _init_(self):
+        self.elements = deque()
+
+    def enqueue(self, element):
+        self.elements.append(element)
+
+    def dequeue(self):
+        return self.elements.popleft()
