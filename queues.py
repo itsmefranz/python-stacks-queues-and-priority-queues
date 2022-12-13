44f1ac22b0901567c6b846c92865700b2d803791
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-14 03:26:02 +0800

    initialized a stack class as per the modules flow of lessons

--- queues.py
+++ queues.py
@@ -9,3 +9,15 @@ class Iterable:
     def _iter_(self):
         while len(self) > 0:
             yield self.dequeue()
+
+class Queue(Iterable):
+    def _init_(self, *elements):
+        self.elements = deque(elements)
+
+    def enqueue(self, element):
+        self.elements.append(element)
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
