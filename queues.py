4e5b6082516a8017fcb7be850e3c964c5d62d9b8
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-13 20:06:33 +0800

    improved Queue class making it iterable and able to report its length and optionally accept initial elements

--- queues.py
+++ queues.py
@@ -3,11 +3,19 @@
 from collections import deque
 
 class Queue:
-    def _init_(self):
-        self.elements = deque()
+    def _init_(self, *elements):
+        self.elements = deque(elements)
+    
+    def _len_(self):
+        return len(self._elements)
+
+    def _iter_(self):
+        while len(self) > 0:
+            yield self.dequeue()
 
     def enqueue(self, element):
         self.elements.append(element)
 
     def dequeue(self):
         return self.elements.popleft()
+
