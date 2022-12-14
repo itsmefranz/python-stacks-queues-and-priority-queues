d146bbad0c340690a8b66ca7d65dabf83a52294c
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-14 13:18:28 +0800

    implemented tuple comparison to determine if the components are true or false

--- implementing-queues/heap.py
+++ implementing-queues/heap.py
@@ -12,3 +12,11 @@ print(fruits)
 heappop(fruits)
 
 print(fruits)
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+print(person1 < person2)
+
+print(person2 < person3)
