55b9207ed1d00093a1b3ff72e5fb54c8aa1cd263
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-15 00:39:16 +0800

    initialized a priority queue and enqueued messages within those priorities

--- implementing-queues/priority-queue.py
+++ implementing-queues/priority-queue.py
@@ -3,3 +3,9 @@ from queues import PriorityQueue
 CRITICAL = 3
 IMPORTANT = 2
 NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio Station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depress")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
