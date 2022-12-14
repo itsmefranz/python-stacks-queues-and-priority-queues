7b8cadddc0e648ea262f7d3b2dabda4325b9909a
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-15 06:05:04 +0800

    fixed a letter in the import class and made a queue_type dictionary

--- thread-safe-queues/thread-safe-queues.py
+++ thread-safe-queues/thread-safe-queues.py
@@ -1,2 +1,8 @@
 import argparse
-from queue import LifoQueue, priorityQueue, Queue
+from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
