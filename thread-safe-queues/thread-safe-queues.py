7323ff9a53f49d51a23c9ec5d3de32a3bd2f76aa
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-15 18:52:17 +0800

    declared a prioritized products variable and stated a dataclass property

--- thread-safe-queues/thread-safe-queues.py
+++ thread-safe-queues/thread-safe-queues.py
@@ -38,11 +38,25 @@ PRODUCTS = (
     ":thread:",
     ":yo-yo:",
 )
+ 
+@dataclass(order=True)
+class Product:
+    priority: int
+    label: str = field(compare=False)
+
+    def __str__(self):
+        return self.label
 
 class Priority(IntEnum):
     HIGH = 1
     MEDIUM = 2
     LOW = 3
+
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW, ":3rd_place_medal:"),
+)
 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
