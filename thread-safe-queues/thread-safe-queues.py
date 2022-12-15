312283cc7b490d898be9390dd56d4021334d95b5
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-15 16:34:32 +0800

    a random delay in seconds adjusted to the worker’s speed and progresses through the work

--- thread-safe-queues/thread-safe-queues.py
+++ thread-safe-queues/thread-safe-queues.py
@@ -66,3 +66,11 @@ def simulate_idle(self):
         self.working = False
         self.progress = 0
         sleep(randint(1, 3))
+
+def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay / 100)
+            self.progress += 1
