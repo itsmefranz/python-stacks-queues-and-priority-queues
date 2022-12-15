351a74baf125bd85e6a6db3c8ca3cf3fef6ae3b5
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-15 19:51:42 +0800

    added a class City with namedtuple

--- uk-roadmap/graph.py
+++ uk-roadmap/graph.py
@@ -2,5 +2,11 @@ import networkx  as nx
 from typing import NamedTuple
 
 graph = nx.nx_agraph.read_dot("roadmap.dot")
-
 graph.nodes["london"]
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
