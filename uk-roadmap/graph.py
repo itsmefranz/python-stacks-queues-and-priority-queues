c50319e7ad6678269e7cb8e5444f15fa438f9678
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-17 15:02:47 +0800

    defined dijkstra shortest path function

--- uk-roadmap/graph.py
+++ uk-roadmap/graph.py
@@ -139,6 +139,15 @@ def search(traverse, graph, source, predicate, order_by=None):
         if predicate(node):
             return node
 
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 # READING DOT FILE TEST
